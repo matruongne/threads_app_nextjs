64ee71b231117a75c467bd3dff48478ff5dded4a
matruongne <matruong052003@gmail.com>
2024-01-04 21:59:46 +0700

    commit3

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,3 @@
-import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google'
 import '../globals.css'
 
@@ -11,10 +10,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<ClerkProvider>
-			<html lang="en">
-				<body className={`${inter.className} bg-dark-1`}>{children}</body>
-			</html>
-		</ClerkProvider>
+		<html lang="en">
+			<body className={`${inter.className} bg-dark-1`}>{children}</body>
+		</html>
 	)
 }
